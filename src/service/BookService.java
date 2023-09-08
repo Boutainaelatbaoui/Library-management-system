5acb6f0b9800ba5c031de3f3b264b5e1a65a28e6
Boutaina <boutaina.elatbaoui@gmail.com>
2023-09-08 10:54:28 +0100

    Added printBookDetails method to BookService to optimize the code

--- src/service/BookService.java
+++ src/service/BookService.java
@@ -1,7 +1,6 @@
 package service;
-import domain.entities.Author;
+
 import domain.entities.Book;
-import repository.BookRepository;
 import java.util.List;
 
 public class BookService {
@@ -10,42 +9,33 @@ public class BookService {
             System.out.println("No book found.");
         } else {
             System.out.println("The book:");
-            System.out.println("Title: " + book.getTitle());
-            System.out.println("Description: " + book.getDescription());
-            System.out.println("Publication Year: " + book.getPublicationYear());
-            System.out.println("ISBN: " + book.getIsbn());
-            System.out.println("Quantity: " + book.getQuantity());
-
-            Author author = book.getAuthor();
-            System.out.println("Author Name: " + author.getName());
-            System.out.println("Author Biography: " + author.getBiography());
-            System.out.println("Author Birthdate: " + author.getBirthdate());
-
+            printBookDetails(book);
             System.out.println();
         }
     }
+
     public void displayBooks(List<Book> books) {
         if (books.isEmpty()) {
             System.out.println("No books found.");
         } else {
             System.out.println("List of Books:");
             for (Book book : books) {
-                System.out.println("Title: " + book.getTitle());
-                System.out.println("Description: " + book.getDescription());
-                System.out.println("Publication Year: " + book.getPublicationYear());
-                System.out.println("ISBN: " + book.getIsbn());
-                System.out.println("Quantity: " + book.getQuantity());
-
+                printBookDetails(book);
                 System.out.println("######################");
-
-                Author author = book.getAuthor();
-                System.out.println("Author Name: " + author.getName());
-                System.out.println("Author Biography: " + author.getBiography());
-                System.out.println("Author Birthdate: " + author.getBirthdate());
-
-                System.out.println();
             }
         }
     }
 
+    private void printBookDetails(Book book) {
+        System.out.println("Title: " + book.getTitle());
+        System.out.println("Description: " + book.getDescription());
+        System.out.println("Publication Year: " + book.getPublicationYear());
+        System.out.println("ISBN: " + book.getIsbn());
+        System.out.println("Quantity: " + book.getQuantity());
+
+        System.out.println("Author Name: " + book.getAuthor().getName());
+        System.out.println("Author Biography: " + book.getAuthor().getBiography());
+        System.out.println("Author Birthdate: " + book.getAuthor().getBirthdate());
+    }
 }
+
